dfc71de491a43a1ba5f0b19a5f547a3f0744de14
jdmunoz21 <jdmunoz21@students.desu.edu>
2025-03-13 15:51:12 -0400

    Screen-sized rooms
    I added a scene called "testRoom" that shows what a room looks like at screen size with the current tileset.

---
+++ scripts/player.gd
@@ -0,0 +1,10 @@
+extends CharacterBody2D
+
+const SPEED = 200
+
+func _physics_process(delta):
+	var direction = Input.get_vector("left", "right", "up", "down")
+	
+	velocity = direction * SPEED
+	
+	move_and_slide()
